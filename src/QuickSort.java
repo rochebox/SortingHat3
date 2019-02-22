a30cb8fdf676b3bd4f8bd964b8b89765ed1be23b
Mr. Roche <rochebox@gmail.com>
2019-02-22 14:56:33 -0500

    Adding Quicksort

---
+++ src/QuickSort.java
@@ -0,0 +1,16 @@
+
+public class QuickSort extends PapaSort
+{
+
+    public QuickSort(int[] l, String sortName) {
+        super(l, sortName);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void executeAlgorithm() {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
